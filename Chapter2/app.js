2a4cd5af1844cd6201923898c79ed2e3a6bc1b0f
DaniyalAhmed585 <daniyalahmed1010@gmail.com>
2020-06-07 16:53:52 +0500

    Ready to Commit

---
+++ Chapter2/app.js
@@ -0,0 +1,48 @@
+                                        // TASK 01
+// var userName;
+
+
+                                        // TASK 02
+// var myName = "Daniyal Ahmed Khan";
+
+
+                                        // TASK 03
+// var message = "Hello World!";
+// alert(message);
+
+
+                                        // TASK 04
+// var studentName = prompt("Enter your name:");
+// var studentAge = prompt("Enter your age:");
+// var studentDegree = prompt("Enter your degree/certification:");
+// alert(studentName);
+// alert(studentAge);
+// alert(studentDegree);
+
+
+                                        // TASK 05
+// var a = prompt("Enter any word: ");
+// while(a.length != 0){
+//     alert(a);
+//     a = a.slice(0, a.length - 1);
+// }
+
+
+                                        // TASK 06
+// var email = "daniyalahmed1010@gmail.com";
+// alert("My email address is " + email);
+
+
+                                        // task 07
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book);
+
+
+                                        // TASK 08
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+                                        // TASK 09
+// var design = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+// document.write(design);
---
+++ Chapter4/app.js
@@ -0,0 +1,15 @@
+                                        // TASK 01
+// var a , b , c;
+
+
+                                        // TASK 02
+//var name , password1 , $userInput , _myBook , userAlert ;  // LEGAL VARIABLE NAMES 
+// var 1 , 1A , a b , "a"a" , prompt ;  // ILLEGAL VARIABLE NAMES
+
+
+                                        // TASK 03
+// document.write("<b> Rules for naming JS variables </b> <br><br><br>");
+// document.write("Variable names can only contain letters, numbers, $ and _ . For example : $my_1stVariable <br>");
+// document.write("Variable must begin with a letter, $ or _ . For example : $name, _name or name <br>");
+// document.write("Variable names are case sensitive <br>");
+// document.write("Variable names should not be JS keywords");
